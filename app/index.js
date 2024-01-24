42bcbb3d1f135c41e789fbf703e5c6d21cdcacfc
Zakaria-Bnh <zakariabenhanna2002@gmail.com>
2024-01-24 01:29:56 +0100

    code imporvment

--- app/index.js
+++ app/index.js
@@ -7,25 +7,33 @@ const overlay = document.querySelector(".overlay");
 const cartIcon = document.querySelector(".open-cart");
 const cartCard = document.querySelector(".cart-card");
 const mainProductImage = document.querySelector(".main-product-image");
-const galleryImages = [...document.querySelectorAll(".gallery-container li img")];
+const galleryImages = [
+  ...document.querySelectorAll(".gallery-container li img"),
+];
+const cartItem = document.querySelector(".cart-item");
+const previousImage = document.querySelector(".previous-image");
+const nextImage = document.querySelector(".next-image");
 
 // Counter for purchases
 const cartPurchases = document.querySelector(".cart-purchases");
 const quantityCounter = document.querySelectorAll(".quantity");
-const quantityCounterContainer = document.querySelector('.quantity-btn-container');
+const quantityCounterContainer = document.querySelector(
+  ".quantity-btn-container"
+);
 const increaseAmount = document.querySelector(".plus-btn");
 const decreaseAmount = document.querySelector(".minus-btn");
 const addToCart = document.querySelector(".add-to-cart");
-let quantity = parseInt(quantityCounter.textContent) || 0;
+const deleteFromCart = document.querySelector(".cart-item-delete");
+console.log(deleteFromCart);
+let quantity = 0;
 
 // Functions for quantity counter
-function increaseQuantityBtn() {
+function increaseQuantity() {
   ++quantity;
-  // cartPurchases.textContent = quantity;
   quantityCounter.forEach((element) => (element.textContent = quantity));
 }
 
-function decreaseQuantityBtn() {
+function decreaseQuantity() {
   if (quantity > 0) {
     --quantity;
     cartPurchases.textContent = quantity;
@@ -33,25 +41,39 @@ function decreaseQuantityBtn() {
   }
 }
 
-// Event listeners for quantity counter
-increaseAmount.addEventListener("click", increaseQuantityBtn);
-decreaseAmount.addEventListener("click", decreaseQuantityBtn);
-
 let itemAddedToCart = true;
-addToCart.addEventListener("click", () => {
-  if (itemAddedToCart && quantity > 0) {
-    // Update UI for item added to cart
-    addToCart.textContent = "Item added to cart!";  
-  } else if (itemAddedToCart ) {
-    increaseQuantityBtn();
+function handleAddToCartClick() {
+  if (quantity < 1) {
+    increaseQuantity();
   }
   [addToCart, quantityCounterContainer].forEach((element) => {
     element.classList.add("item-added");
   });
+  cartItem.classList.remove("hidden");
+  cartItem.classList.add("flex-container");
+  addToCart.textContent = "Item added to cart!";
   cartPurchases.textContent = quantity;
-  // itemAddedToCart = true;
-});
+}
 
+function handleDeleteFromCart() {
+  addToCart.textContent = "Add to cart";
+  cartItem.classList.add("hidden");
+  cartItem.classList.remove("flex-container");
+
+  [addToCart, quantityCounterContainer].forEach((element) => {
+    element.classList.remove("item-added");
+  });
+  [cartPurchases, ...quantityCounter].forEach((e) => {
+    e.textContent = 0;
+    quantity = 0;
+  });
+}
+
+// Event listeners for quantity counter
+increaseAmount.addEventListener("click", increaseQuantity);
+decreaseAmount.addEventListener("click", decreaseQuantity);
+addToCart.addEventListener("click", handleAddToCartClick);
+deleteFromCart.addEventListener("click", handleDeleteFromCart);
 // Image sourceset array
 const imagesSourceset = [
   "images/image-product-1.jpg",
@@ -61,11 +83,15 @@ const imagesSourceset = [
 ];
 
 // Event listeners for gallery images
+let imageIndex;
+
 galleryImages.forEach((image) => {
   image.addEventListener("click", () => {
     mainProductImage.src = image.src;
   });
 });
+console.log(mainProductImage.src);
+nextImage.addEventListener("click", () => {});
 
 // Event listener for mobile/desktop menu
 hamburgerMenu.addEventListener("click", () => {
